9c68ec2be5a75d4b4c866fb059fa7245fe49a807
Dima Tarasenko <taras3nko.dima@gmail.com>
2021-03-11 14:15:52 +0000

    basic edits, benchmark

--- twint/datelock.py
+++ twint/datelock.py
@@ -25,15 +25,16 @@ def convertToDateTime(string):
 def Set(Until, Since):
     logme.debug(__name__+':Set')
     d = Datelock()
-
+    d.until = Until
+    d.since = Since
     if Until:
-        d.until = datetime.datetime.strptime(convertToDateTime(Until), "%Y-%m-%d %H:%M:%S")
+        d.until = datetime.datetime.strptime(Until, "%Y-%m-%d")
         d.until = utc_to_local(d.until)
     else:
         d.until = datetime.datetime.today()
 
     if Since:
-        d.since = datetime.datetime.strptime(convertToDateTime(Since), "%Y-%m-%d %H:%M:%S")
+        d.since = datetime.datetime.strptime(Since, "%Y-%m-%d")
         d.since = utc_to_local(d.since)
         d._since_def_user = True
     else:
